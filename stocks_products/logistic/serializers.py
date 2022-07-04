0cc4b61430668623c87881299e2f7f124c102887
Sergei Bykov <ss.bykoff@gmail.com>
2022-07-04 12:45:05 +0300

    первый коммит

---
+++ stocks_products/logistic/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StockProduct
+        fields = '__all__'
+
+    def validate_empty_values(self, data):
+        return True, data
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['address', 'positions']
+
+    def create(self, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().create(validated_data)
+
+        for position in positions:
+            StockProduct(
+                quantity=position['quantity'],
+                price=position['price'],
+                product_id=position['product'],
+                stock_id=stock.id
+            ).save()
+        return stock
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+
+        stock = super().update(instance, validated_data)
+        for position in positions:
+            StockProduct.objects.update_or_create(
+                stock=stock,
+                product=Product.objects.get(id=position['product']),
+                defaults={'price': position['price'], 'quantity': position['quantity']}
+            )
+        return stock
+
---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,39 @@
+from django_filters.rest_framework import DjangoFilterBackend, CharFilter, FilterSet
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter, DjangoFilterBackend]
+    search_fields = ['title', 'description']
+
+
+class StockFilter(FilterSet):
+    products = CharFilter(field_name='products')
+
+    class Meta:
+        model = Stock
+        fields = '__all__'
+
+class StockSearchFilter(SearchFilter):
+    search_param = "products"
+
+
+class StockPagination(PageNumberPagination):
+    page_size = 3
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = StockFilter
+    search_fields = ['products__title', 'products__description']
+    pagination_class = StockPagination
+
